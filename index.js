946e1a71d9848477fc62e4403a6806236648be21
czyin <czyin@users.noreply.github.com>
2017-02-19 09:53:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
+"use strict";
+
 // exports = module.exports = !!(typeof process !== "undefined" && {}.toString.call(process) === "[object process]");
 module.exports = typeof process !== "undefined" && {}.toString.call(process) === "[object process]";
